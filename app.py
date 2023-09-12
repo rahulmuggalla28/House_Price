5a4e0b98190de85a86257bdd577bd3a0adb651cf
rahulmuggalla28 <93699515+rahulmuggalla28@users.noreply.github.com>
2023-09-12 17:09:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+# pip install flask
+
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+
+with open('house_prediction_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['post'])
+def predict():
+    bed = int(request.form['bedrooms'])
+    bath = int(request.form['bathrooms'])
+    loc = int(request.form['location'])
+    size = int(request.form['size'])
+    status = int(request.form['status'])
+    face = int(request.form['facing'])
+    Type = int(request.form['type'])
+    
+    input_data = np.array([[bed, bath, loc, size, status, face, Type]])
+    
+    predicted_price = model.predict(input_data)[0]
+    
+    return render_template('index.html', predicted_price = predicted_price)
+
+app.run(debug=True)
